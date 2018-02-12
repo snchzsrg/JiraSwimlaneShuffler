45749d443250735047472baade23de2086cf3d11
Sergio Sanchez <snchz.srg@gmail.com>
2018-02-11 20:39:12 -0800

    Fix formatting issues and delete unneeded popup.html file

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.executeScript({
     file: 'shuffle.js'
   });
--- shuffle.js
+++ shuffle.js
@@ -3,8 +3,8 @@ var divs = document.getElementsByClassName("shufflable");
 var index, randomIndex;
 var temp;
 for (index = divs.length - 1; index >= 0; index--) {
-randomIndex = Math.floor(Math.random() * (index + 1));
-temp = divs[index].innerHTML;
-divs[index].innerHTML = divs[randomIndex].innerHTML;
-divs[randomIndex].innerHTML = temp;
+    randomIndex = Math.floor(Math.random() * (index + 1));
+    temp = divs[index].innerHTML;
+    divs[index].innerHTML = divs[randomIndex].innerHTML;
+    divs[randomIndex].innerHTML = temp;
 }
